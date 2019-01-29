0b7c6cba6cb2ef4a8f2b7c2dbed9494af1a9db5a
Nick Osiunin <mykola_osiunin@epam.com>
2019-01-29 03:45:31 +0100

    Refactoring

--- readme.md
+++ readme.md
@@ -1,6 +1,9 @@
 # NodeJS mentoring. Homework application
 
 Run solution:
-npm i && npm start
-
-check sync/asyn option in _config/json_
+npm run streams -- --help  
+npm run streams -- --action transform  
+npm run streams -- --action reverse  
+npm run streams -- -a outputFile -f ./package.json  
+npm run streams -- -a convertFromFile -f ./data/new-test-file.csv  
+npm run streams -- -a convertToFile -f ./data/new-test-file.csv  
--- src/utils/streams.js
+++ src/utils/streams.js
@@ -11,125 +11,137 @@ const {
   OUTPUTFILE_MSG,
   NOFILE_MSG,
   TRANSFORM_MSG,
-  CONVERTFILE_MSG
+  CONVERTFILE_MSG,
+  CONVERTFILE_INTRO_MSG,
 } = messages
-const {argv} = process
-const CMD = {
+const ARGS_SUPPORTED = {
   HELP: 'help',
   ACTION: 'action',
   FILE: 'file',
 }
-const minimistOptions = {
-  string: [CMD.ACTION, CMD.FILE],
-  boolean: CMD.HELP,
-  alias: {h: CMD.HELP, a: CMD.ACTION, f: CMD.FILE}
+const ACTIONS = {
+  REVERSE: 'reverse',
+  TRANSFORM: 'transform',
+  OUTPUTFILE: 'outputFile',
+  CONVERTFROMFILE: 'convertFromFile',
+  CONVERTTOFILE: 'convertToFile',
 }
+const minimistOptions = {
+  string: [ARGS_SUPPORTED.ACTION, ARGS_SUPPORTED.FILE],
+  boolean: ARGS_SUPPORTED.HELP,
+  alias: {h: ARGS_SUPPORTED.HELP, a: ARGS_SUPPORTED.ACTION, f: ARGS_SUPPORTED.FILE}
+}
+
+const {argv} = process
 const parsedArgs = minimist(argv.slice(2), minimistOptions);
 const {help, action, file} = parsedArgs
+const actionsInfo = {
+  [ACTIONS.REVERSE]: {
+    introMsg: REVERSE_MSG,
+    modifier: str => str.split('').reverse().join(''),
+  },
+  [ACTIONS.TRANSFORM]: {
+    introMsg: TRANSFORM_MSG,
+    modifier: str => str.toUpperCase(),
+  },
+  [ACTIONS.OUTPUTFILE]: {
+    introMsg: `${OUTPUTFILE_MSG} ${file}:\n`,
+    handleStream: stream => stream.pipe(process.stdout),
+    fileRequired: true,
+  },
+  [ACTIONS.CONVERTFROMFILE]: {
+    introMsg: `${OUTPUTFILE_MSG} ${file} in JSON:\n`,
+    handleStream: stream => jsonFromCsv(stream).pipe(process.stdout),
+    fileRequired: true,
+  },
+  [ACTIONS.CONVERTTOFILE]: {
+    introMsg: `${CONVERTFILE_INTRO_MSG} ${file}:`,
+    handleStream: stream => jsonFromCsv(stream).pipe(getWriteStream(file)),
+    fileRequired: true,
+  },
+}
+
+const CTRLC_MSG = `\n\nAction: "${action}" successfully performed.\n`
+const fileRelatedActions = Object.entries(actionsInfo)
+  .filter(item => item[1].fileRequired)
+  .map(item => item[0])
+
+const directInputActions = Object.entries(actionsInfo)
+  .filter(item => !item[1].fileRequired)
+  .map(item => item[0])
+
 const noArgs = () => argv.length < 3
 const helpIsFirst = () => noArgs()
   ? false
-  : help && [CMD.HELP, 'h'].includes(argv.slice(2, 3).pop().split('-').pop())
+  : help && [ARGS_SUPPORTED.HELP, 'h'].includes(argv.slice(2, 3).pop().split('-').pop())
+const unknownAction = () => !Object.values(ACTIONS).includes(action)
+const convertedFileName = file => file.split('/').pop().split('.csv').shift() + '.json'
 
-if (noArgs() || helpIsFirst()) {
+process.on('SIGINT', function () {
+  console.log(CTRLC_MSG)
+  process.exit(0)
+})
+
+// Print commands list
+if (noArgs() || unknownAction() || helpIsFirst()) {
   console.log(HELP_MSG)
-} else {
-  switch (action) {
-    case 'outputFile':
-      if (!file) {
-        console.log(NOFILE_MSG)
-        break
-      }
-      console.log(`${OUTPUTFILE_MSG} ${file}:\n\n`)
-      outputFile()
-      break
-    case 'convertFromFile':
-      if (!file) {
-        console.log(NOFILE_MSG)
-        break
-      }
-      console.log(`${OUTPUTFILE_MSG} ${file} in JSON:\n\n`)
-      convertFromFile()
-      break;
-    case 'convertToFile':
-      if (!file) {
-        console.log(NOFILE_MSG)
-        break
-      }
-      convertToFile()
-      break;
-    case 'transform':
-      console.log(TRANSFORM_MSG)
-      transform()
-      break
-    case 'reverse':
-    default:
-      console.log(REVERSE_MSG)
-      reverse()
-  }
+  process.exit(0)
 }
 
-
-function reverse() {
-  const reverseStream = through(write, end);
-
-  function write(buffer, encoding, next) {
-    this.push(buffer.toString().split('').reverse().join('') + "\n")
-    next()
-  }
-
-  function end(done) {
-    done()
-  }
-
-  process.stdin.pipe(reverseStream).pipe(process.stdout);
+// User direct input actions
+if (directInputActions.includes(action)) {
+  console.log(actionsInfo[action].introMsg)
+  transformUserInput(action)
 }
 
-function transform() {
-  const transformStream = through(write, end);
-
-  function write(buffer, encoding, next) {
-    this.push(buffer.toString().toUpperCase() + "\n")
-    next()
-  }
-
-  function end(done) {
-    done()
-  }
-
-  process.stdin.pipe(transformStream).pipe(process.stdout);
-}
-
-function outputFile() {
-  fs.createReadStream(path.resolve(file))
-    .on('error', function (err) {
-      if (err.code === 'ENOENT') console.log(NOFILE_MSG)
-    })
-    .pipe(process.stdout);
-}
-
-function convertFromFile() {
-  fs.createReadStream(path.resolve(file))
-    .on('error', function (err) {
-      if (err.code === 'ENOENT') console.log(NOFILE_MSG)
-    })
-    .pipe(csvtojson())
-    .pipe(process.stdout)
-}
-
-function convertToFile() {
-  const csvFile = path.resolve(file);
-  const jsonFile = csvFile.split('/').pop().split('.csv').shift() + '.json'
-  const writeJsonStream = fs.createWriteStream(jsonFile);
-
-  fs.createReadStream(csvFile)
-    .on('error', function (err) {
-      if (err.code === 'ENOENT') console.log(NOFILE_MSG)
-    })
-    .pipe(csvtojson())
-    .pipe(writeJsonStream)
+// File related actions
+if (fileRelatedActions.includes(action)) {
+  console.log(actionsInfo[action].introMsg)
+  const readStream = getStreamOrDie(file)
+  actionsInfo[action]
+    .handleStream(readStream)
     .on('close', () => {
-      console.log(`${CONVERTFILE_MSG}${jsonFile}\n`)
+      console.log(`${CONVERTFILE_MSG}${convertedFileName(file)}\n`)
     })
 }
 
+function transformUserInput(action) {
+  const finalStream = through(write);
+  const modifyBuffer = (buffer) =>
+    actionsInfo[action]
+      .modifier(buffer.toString()) + "\n"
+
+  process.stdin
+    .pipe(finalStream)
+    .pipe(process.stdout);
+
+  function write(buffer, encoding, next) {
+    this.push(modifyBuffer(buffer))
+    next()
+  }
+}
+
+function getStreamOrDie(filename) {
+  if (!filename) {
+    fileAccessError()
+  }
+  return fs.createReadStream(path.resolve(filename))
+    .on('error', function (err) {
+      if (err.code === 'ENOENT') {
+        fileAccessError()
+      }
+    })
+}
+
+function fileAccessError() {
+  console.log(NOFILE_MSG)
+  process.exit(0);
+}
+
+function jsonFromCsv(stream) {
+  return stream.pipe(csvtojson())
+}
+
+function getWriteStream(file) {
+  return fs.createWriteStream(convertedFileName(file));
+}
